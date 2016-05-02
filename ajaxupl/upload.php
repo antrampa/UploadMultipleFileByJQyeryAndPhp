9c538ad6d54a7a256fa90133b18061bd86b0b90d
ambitos-hp <antrampa@gmail.com>
2016-05-02 19:40:47 +0300

    Auto submit

--- ajaxupl/upload.php
+++ ajaxupl/upload.php
@@ -1,61 +1,83 @@
 <?php
-/*
-Server-side PHP file upload code for HTML5 File Drag & Drop demonstration
-Featured on SitePoint.com
-Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
-*/
-$fn = (isset($_SERVER['HTTP_X_FILENAME']) ? $_SERVER['HTTP_X_FILENAME'] : false);
-//$postdata = file_get_contents('php://input');
-//die(var_dump($postdata));
-$uplfr2 = $_POST['uplfr'];
-$uplfr = $_POST['X_UPLFR'];//(isset($_SERVER['uplfr']) ? $_SERVER['uplfr'] : false);
-echo "uplfr:" . $uplfr;
-echo "<br>uplfr2:" . $uplfr2;
-die("<br>uplfr:" . $uplfr);
-if ($fn) {
 
-	// AJAX call
-	//file_put_contents(
-	//	'uploadedocs/' . $fn,
-	//	file_get_contents('php://input')
-	//);
-	
-	if(!is_dir($uplfr))
-	{
-		if (!mkdir($uplfr, 7777, true)) {
-			die('Failed to create folders...');
-		}
-	}
-	
-	file_put_contents( $uplfr.'/'. $fn, file_get_contents('php://input'));
-	
-	echo $fn;
-    echo '/'.$_POST["uplfr"].'/'. $fn;
-    echo '/'.$uplfr.'/'. $fn;
-	exit();
+$uplfr = $_POST['uplfr'];
+//$files = $_FILES['file']; //fileselect
+$files = $_FILES['fileselect'];
+//die(print_r($files));
 
+if(!is_dir($uplfr))
+{
+    if (!mkdir($uplfr, 7777, true)) {
+        die('Failed to create folders...');
+    }
 }
-else {
 
-	// form submit
-	$files = $_FILES['fileselect'];
-
-	if(!is_dir($uplfr))
-	{
-		if (!mkdir($uplfr, 7777, true)) {
-			die('Failed to create folders...');
-		}
-	}
-	
-	foreach ($files['error'] as $id => $err) {
-		if ($err == UPLOAD_ERR_OK) {
-			$fn = $files['name'][$id];
-			move_uploaded_file(
-				$files['tmp_name'][$id],
-				'uploadedocs/'. $uplfr.'/'. $fn
-			);
-			echo "<p>File $fn uploaded.</p>";
-		}
-	}
-
+foreach ($files['error'] as $id => $err)
+{
+    if ($err == UPLOAD_ERR_OK)
+    {
+        $fn = $files['name'][$id];
+        move_uploaded_file(
+            $files['tmp_name'][$id],
+            $uplfr. "/" . $fn
+        );
+        echo "<p>File $fn uploaded.</p>";
+    }
 }
+
+//foreach ($files as $file)
+//{
+//    //move_uploaded_file($file['tmp_name'], $uplfr."/" . $file['name']);
+//    echo "<p>File ".$file['name']." uploaded.</p>";
+//}
+
+//if ($fn)
+//{
+//
+//    // AJAX call
+//    //file_put_contents(
+//    //	'uploadedocs/' . $fn,
+//    //	file_get_contents('php://input')
+//    //);
+//
+//    if(!is_dir($uplfr))
+//    {
+//        if (!mkdir($uplfr, 7777, true)) {
+//            die('Failed to create folders...');
+//        }
+//    }
+//
+//    file_put_contents( $uplfr.'/'. $fn, file_get_contents('php://input'));
+//
+//    echo $fn;
+//    echo '/'.$_POST["uplfr"].'/'. $fn;
+//    echo '/'.$uplfr.'/'. $fn;
+//    exit();
+//
+//}
+//else
+//{
+//
+//    if(!is_dir($uplfr))
+//    {
+//        if (!mkdir($uplfr, 7777, true)) {
+//            die('Failed to create folders...');
+//        }
+//    }
+//
+//    //$files = $_FILES['file']; //fileselect
+//    $file = $_FILE['filename'];
+//    move_uploaded_file($_FILES['filename']['tmp_name'], $uplfr."/" . $_FILES['filename']['name']);
+//    echo "<p>File $fn uploaded.</p>";
+//
+////    foreach ($files['error'] as $id => $err) {
+////        if ($err == UPLOAD_ERR_OK) {
+////            $fn = $files['name'][$id];
+////            move_uploaded_file(
+////                $files['tmp_name'][$id],
+////                'uploadedocs/'. $uplfr.'/'. $fn
+////            );
+////            echo "<p>File $fn uploaded.</p>";
+////        }
+////    }
+//}
--- filedrag1/fileuploader.js
+++ filedrag1/fileuploader.js
@@ -5,6 +5,7 @@ $(document).ready(function ()
 {
     $("body").on("click", "#submit", function() {
         $("#upload").submit(function(e) {
+            // alert('on submit');
             var formObj = $(this);
             var formURL = formObj.attr("action");
             var formData = new FormData(this);
@@ -27,5 +28,104 @@ $(document).ready(function ()
             e.unbind();
         });
     });
+
+    function $id(id) {
+        return document.getElementById(id);
+    }
+
+    function Output(msg) {
+        var m = $id("messages");
+        m.innerHTML = msg + m.innerHTML;
+    }
+
+
+    // file drag hover
+    function FileDragHover(e) {
+        // alert('inside FileDragHover');
+        e.stopPropagation();
+        e.preventDefault();
+        e.target.className = (e.type == "dragover" ? "hover" : "");
+    }
+
+
+    // file selection
+    function FileSelectHandler(e)
+    {
+        // alert('inside FileSelectHandler');
+        // cancel event and hover styling
+        FileDragHover(e);
+
+        // fetch FileList object
+        var files = e.target.files || e.dataTransfer.files;
+
+        // process all File objects
+
+        for (var i = 0, f; f = files[i]; i++)
+        {
+            ParseFile(f);
+            //UploadFile(f);
+        }
+        // alert('inside upload');
+        $("#submit").click();
+        // alert('inside upload');
+    }
+
+
+    // output file information
+    function ParseFile(file) {
+//        alert('Start Parsing file :'+file.name);
+        Output(
+            "<p>File information: <strong>" + file.name +
+            "</strong> type: <strong>" + file.type +
+            "</strong> size: <strong>" + file.size +
+            "</strong> bytes</p>"
+        );
+
+        var path = $('#uplfr').val();
+        // display an image
+        if (file.type.indexOf("image") == 0) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                Output(
+                    "<p><strong>" + file.name + ":</strong><br />" +
+                    '<img src="ajaxupl/' + path + '/' + file.name + '" /></p>'
+                );
+            }
+            reader.readAsDataURL(file);
+        }
+
+        // display text
+        if (file.type.indexOf("text") == 0) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                Output(
+                    "<p><strong>" + file.name + ":</strong></p><pre>" +
+                    e.target.result.replace(/</g, "&lt;").replace(/>/g, ">") +
+                    "</pre>"
+                );
+            }
+            reader.readAsText(file);
+        }
+
+    }
+
+    // initialize
+    function Init() {
+        // alert('inside init');
+
+        var fileselect = $id("fileselect"),
+            filedrag = $id("filedrag"),
+            submitbutton = $id("submitbutton");
+
+        // file select
+        fileselect.addEventListener("change", FileSelectHandler, false);
+    }
+
+    // call initialization file
+    if (window.File && window.FileList && window.FileReader)
+    {
+        Init();
+    }
+
 
 });
