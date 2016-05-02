9d6f20e5debcdc8ff1d71d533a3b5256824b040b
ambitos-hp <antrampa@gmail.com>
2016-05-02 19:09:46 +0300

    g

--- filedrag1/fileuploader.js
+++ filedrag1/fileuploader.js
@@ -27,53 +27,5 @@ $(document).ready(function ()
             e.unbind();
         });
     });
-    //
-    // $("#uploadbutton").click(function ()
-    // {
-    //     $("#upload").submit(function(e) {
-    //         var formObj = $(this);
-    //         var formURL = formObj.attr("action");
-    //         var formData = new FormData(this);
-    //         $.ajax({
-    //             url: formURL,
-    //             type: 'POST',
-    //             data:  formData,
-    //             mimeType: "multipart/form-data",
-    //             contentType: false,
-    //             cache: false,
-    //             processData: false,
-    //             success: function(data, textStatus, jqXHR) {
-    //                 alert('ok');
-    //             },
-    //             error: function(jqXHR, textStatus, errorThrown) {
-    //                 alert('error');
-    //             }
-    //         });
-    //         e.preventDefault();
-    //         e.unbind();
-    //     });
-    // });
 
-    //     var formObj = $(this);
-    //     var formURL = formObj.attr("action");
-    //     var formData = new FormData(this);
-    //     alert(formURL);
-    //     alert(formData);
-    //     $.ajax({
-    //         url: formURL,
-    //         type: 'POST',
-    //         data:  formData,
-    //         mimeType: "multipart/form-data",
-    //         contentType: false,
-    //         cache: false,
-    //         processData: false,
-    //         success: function(data, textStatus, jqXHR) {
-    //             alert('ok: ' + data + ' ' + textStatus + ' ' + jqXHR);
-    //         },
-    //         error: function(jqXHR, textStatus, errorThrown) {
-    //             alert('error');
-    //             alert('error: ' + jqXHR + ' ' + textStatus + ' ' + errorThrown);
-    //         }
-    //     });
-    // });
 });
